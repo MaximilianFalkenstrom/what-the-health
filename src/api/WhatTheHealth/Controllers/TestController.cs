3b4d5c9e68fe38a4a3e6accdf873c4a4acccf382
Maximilian Falkenström <maximilianfalkenstrom@gmail.com>
2024-01-20 17:07:28 +0100

    feature: add auth

---
+++ src/api/WhatTheHealth/Controllers/TestController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WhatTheHealth.API.Controllers;
+[Route("api/[controller]")]
+[ApiController]
+public class TestController : ControllerBase
+{
+    [HttpGet("unauthorized")]
+    public IActionResult Unauthorized()
+    {
+        return Ok("Unauthorized request finished successfully.");
+    }
+
+    [HttpGet("authorized")]
+    [Authorize]
+    public IActionResult Authorized()
+    {
+        return Ok("Authorized request finished successfully");
+    }
+}
